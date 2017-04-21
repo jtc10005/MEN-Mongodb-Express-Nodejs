893f01bef945b102b97db6247c5d1751ffe45840
Jay Culpepper <jay.culpepper@ldcom.com>
2017-04-21 14:42:32 -0500

    saving and display results in console

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 const express = require('express')
-const bodyParser= require('body-parser')
+const bodyParser = require('body-parser')
 const app = express()
 const MongoClient = require('mongodb').MongoClient
 
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 
 var mgdb = "mongodb://MEN_TEST:MEN_TEST!@ds139899.mlab.com:39899/mongo_node";
 console.log('root: ' + __dirname);
@@ -11,19 +11,29 @@ console.log('root: ' + __dirname);
 var db
 
 MongoClient.connect(mgdb, (err, database) => {
-  if (err) return console.log(err)
-  db = database
-  app.listen(3000, () => {
-    console.log('listening on 3000')
-  })
+    if (err) return console.log(err)
+    db = database
+    app.listen(3000, () => {
+        console.log('listening on 3000')
+    })
 })
 
-app.get('/',  (req, res) => {
+app.get('/', (req, res) => {
     console.log('get request');
+    var cursor = db.collection('quotes').find().toArray(function (err, results) {
+        console.log(results)
+        // send HTML file populated with quotes here
+    })
     res.sendFile(__dirname + '/index.html');
 })
 
 app.post('/quotes', (req, res) => {
+    console.log('Post recieved', req.body);
 
-  console.log('Post recieved',req.body);
-})
+    db.collection('quotes').save(req.body, (err, result) => {
+        if (err) return console.log(err)
+
+        console.log('saved to database')
+        res.redirect('/')
+    })
+});
