622643bd3497ba882bdeb8be3c454db5e6d82833
Jay Culpepper <jay.culpepper@ldcom.com>
2017-04-21 14:35:07 -0500

    mongo connecting

--- src/server.js
+++ src/server.js
@@ -5,8 +5,7 @@ const MongoClient = require('mongodb').MongoClient
 
 app.use(bodyParser.urlencoded({extended: true}))
 
-const mgdb = "mongodb://jtc-group_cluster0:c0nl@n1017@cluster0-shard-00-00-1xhpp.mongodb.net:27017,cluster0-shard-00-01-1xhpp.mongodb.net:27017,cluster0-shard-00-02-1xhpp.mongodb.net:27017/db1?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
-
+var mgdb = "mongodb://MEN_TEST:MEN_TEST!@ds139899.mlab.com:39899/mongo_node";
 console.log('root: ' + __dirname);
 
 var db
@@ -19,7 +18,7 @@ MongoClient.connect(mgdb, (err, database) => {
   })
 })
 
-app.get('/', function (req, res) {
+app.get('/',  (req, res) => {
     console.log('get request');
     res.sendFile(__dirname + '/index.html');
 })
